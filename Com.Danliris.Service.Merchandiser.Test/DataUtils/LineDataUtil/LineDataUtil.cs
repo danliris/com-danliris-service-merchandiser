a0939e43b187f4aafb35da27489c6683c70013af
adityahenanda <henanda.aditya@gmail.com>
2018-05-30 11:06:10 +0700

    add uni test

---
+++ Com.Danliris.Service.Merchandiser.Test/DataUtils/LineDataUtil/LineDataUtil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Merchandiser.Test.DataUtils.LineDataUtil
+{
+    class LineDataUtil
+    {
+    }
+}
